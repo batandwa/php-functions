591760685d88076ea0422e89aef27eb5571b0e60
Batandwa Colani <i@batandwa.me>
2014-05-06 08:54:55 +0200

    Added is_int

---
+++ string.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Custom is_int function that works on strings as well.
+ *
+ * @author  Batandwa Colani
+ * @date    2014-02-04
+ * @version 1
+ *
+ * @param   mixed      $val The potential integer to be evaluated
+ *
+ * @return  bool            Whether the input is an integer or not.
+ */
+function crabtree_core_is_int($val){
+  return (filter_var($val, FILTER_VALIDATE_INT) !== false && strpos($val, '-') === false);
+}
